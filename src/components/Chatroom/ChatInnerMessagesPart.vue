dba90e322ea4037221757d5fc07ff85a380e50cd
Pavle <jovanovicpavle00@gmail.com>
2022-07-12 16:31:11 +0200

    contact sidebar done

---
+++ src/components/Chatroom/ChatInnerMessagesPart.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="messages-and-send-message">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ChatInnerMessagesPart"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ChatInner.vue
+++ src/views/ChatInner.vue
@@ -3,17 +3,26 @@
     <SideBar/>
     <div class="main-div">
       <ChatInnerHeader/>
+      <div class="user-details">
+        <div class="left">
+          <p>Title: </p>
+          <div class="pill">
+          </div>
+        </div>
+      </div>
     </div>
+    <ChatInnerContact/>
   </div>
 </template>
 
 <script>
 import SideBar from "@/components/SideBar";
 import ChatInnerHeader from "@/components/Chatroom/ChatInnerHeader";
+import ChatInnerContact from "@/components/Chatroom/ChatInnerContact";
 
 export default {
   name: "ChatInner",
-  components: {ChatInnerHeader, SideBar}
+  components: {ChatInnerContact, ChatInnerHeader, SideBar}
 }
 </script>
 
@@ -21,16 +30,19 @@ export default {
 .parent {
   display: flex;
   background-color: #171819;
-}
-
-.inner-cont {
-  width: 61rem;
+  justify-content: space-between;
 }
 
 .main-div {
   width: 61rem;
-  padding-left: 10.563rem;
-  padding-right: 9.313rem;
+  margin-left: 10.563rem;
+  margin-right: 9.313rem;
 }
 
+.user-details {
+  width: 60.938rem;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  padding-bottom: 2.938rem;
+  margin-top: 2.438rem;
+}
 </style>
