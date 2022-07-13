26b55194d556822cc20586ef1d0fe8df637f5d1a
Pavle <jovanovicpavle00@gmail.com>
2022-07-13 16:50:10 +0200

    user details done

--- src/views/ChatInner.vue
+++ src/views/ChatInner.vue
@@ -5,16 +5,45 @@
       <div class="chat">
         <ChatInnerHeader/>
         <div class="user-details">
-          <div class="left">
-            <p>Title: </p>
+          <div>
+            <p class="details-title">
+              Title:
+            </p>
             <div class="pill">
+              <p>GM</p>
+            </div>
+            <div class="pill">
+              <p>IO</p>
+            </div>
+            <div class="pill">
+              <p>FA</p>
+            </div>
+            <div class="pill">
+              <p>FI</p>
+            </div>
+          </div>
+          <div>
+            <p class="details-title">
+              Rating :
+            </p>
+            <div class="pill">
+              <p><span>Standard:</span> 2100</p>
+            </div>
+            <div class="pill">
+              <p><span>Rapid :</span> 2100</p>
+            </div>
+            <div class="pill">
+              <p><span>Blitz :</span> 2100</p>
             </div>
           </div>
         </div>
-        <OneMessage message-type="received" />
-        <OneMessage message-type="sent" />
-        <OneMessage message-type="received" />
+        <OneMessage message-type="received"/>
+        <OneMessage message-type="sent"/>
+        <OneMessage message-type="received"/>
       </div>
+      <!--      <div class="send-message">-->
+      <!--        -->
+      <!--      </div>-->
     </div>
     <ChatInnerContact/>
   </div>
@@ -33,6 +62,12 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 .parent {
   display: flex;
   background-color: #171819;
@@ -49,7 +84,7 @@ export default {
 
 .chat {
   height: 80vh;
-  overflow-y: scroll;
+  overflow-y: auto;
 }
 
 .user-details {
@@ -57,5 +92,29 @@ export default {
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   padding-bottom: 2.938rem;
   margin-top: 2.438rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+.user-details div {
+  display: flex;
+  align-items: center;
+}
+
+.details-title {
+  font-size: 1rem;
+  color: #6F7381;
+  font-weight: 500;
+  margin-right: 2rem;
+}
+
+.pill {
+  color: white;
+  padding: 0.438rem 0.75rem;
+  border: 2px solid #C8A07D;
+  border-radius: 20px;
+  margin-right: 0.875rem;
+}
+
 </style>
